49c2d9dc4058bf1e8ffe4429bd7bad45eedf4303
Leivas <rodrigo.leivas@sap.com>
2025-07-31 20:45:52 -0300

    placeImport service and controller created. Tests created

--- src/main/java/com/highwaytoiletfinder/place/repository/PlaceRepository.java
+++ src/main/java/com/highwaytoiletfinder/place/repository/PlaceRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface PlaceRepository extends JpaRepository<Place, UUID> {
+    boolean existsByGooglePlaceId(String googlePlaceId);
 }
---
+++ src/main/java/com/highwaytoiletfinder/placeImport/controller/PlaceImportController.java
@@ -0,0 +1,23 @@
+package com.highwaytoiletfinder.placeImport.controller;
+
+import com.highwaytoiletfinder.googleplaces.model.NearbySearchRequest;
+import com.highwaytoiletfinder.placeImport.service.PlaceImportService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/import")
+@RequiredArgsConstructor
+public class PlaceImportController {
+    private final PlaceImportService placeImportService;
+
+    @PostMapping("/places")
+    public ResponseEntity<Void> importPlaces(@RequestBody NearbySearchRequest request) {
+        placeImportService.importNearbyPlaces(request);
+        return ResponseEntity.ok().build();
+    }
+}
